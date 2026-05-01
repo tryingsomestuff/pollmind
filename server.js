40208970853e21f30e7dbad22fc3d1116d78b26a
Vivien Clauzon <xr_a_y@yahoo.fr>
2026-05-01 14:30:31 +0200

    update

--- server.js
+++ server.js
@@ -33,18 +33,18 @@ app.use((req, res, next) => {
   next();
 });
 
-// Middleware d'authentification
+// Authentication middleware
 const authenticateToken = (req, res, next) => {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
 
   if (!token) {
-    return res.status(401).json({ error: 'Token manquant' });
+    return res.status(401).json({ error: 'Missing token' });
   }
 
   jwt.verify(token, SECRET_KEY, (err, user) => {
     if (err) {
-      return res.status(403).json({ error: 'Token invalide' });
+      return res.status(403).json({ error: 'Invalid token' });
     }
     req.user = user;
     next();
